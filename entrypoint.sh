315d7fd3df9ad45972d6c94dc0baa7bb02e5b4a7
Joe Selman <jsselman@google.com>
2020-02-05 15:02:52 -0800

    use correct name for output

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ if [[ $success -ne 0 ]]; then
     # TODO check if PR and only comment if it is
     comments_url=$(jq -r '.pull_request.comments_url' $GITHUB_EVENT_PATH)
     # Wrap output so it's easier to read in a PR.
-    wrapped_output=$(echo "$comment" | fold -w 80 -s)
+    wrapped_output=$(echo "$output" | fold -w 80 -s)
     comment=$'istioctl analyze found problems:\n\n```\n'"${wrapped_output}"$'\n```\n'
     data=$(echo '{}' | jq --arg body "$comment" '.body = $body')
     curl -s -S \
