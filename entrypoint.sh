66218c70470ee5a3305ab35756c20e502b9c366a
Joe Selman <jsselman@google.com>
2020-02-05 14:52:13 -0800

    Wrap output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set +e
 output=$(find $1 \
     -type f \
     -name \*.yaml \
-    -print0 | xargs -0 istioctl analyze -A -d)
+    -print0 | xargs -0 istioctl analyze -A -d 2>&1)
 success=$?
 echo $output
 set -e
@@ -11,7 +11,9 @@ set -e
 if [[ $success -ne 0 ]]; then
     # TODO check if PR and only comment if it is
     comments_url=$(jq -r '.pull_request.comments_url' $GITHUB_EVENT_PATH)
-    comment=$'istioctl analyze found problems:\n\n```\n'"${output}"$'```\n'
+    # Wrap output so it's easier to read in a PR.
+    wrapped_output=$(echo "$comment" | fold -w 80 -s)
+    comment=$'istioctl analyze found problems:\n\n```\n'"${wrapped_output}"$'\n```\n'
     data=$(echo '{}' | jq --arg body "$comment" '.body = $body')
     curl -s -S \
         -H "Authorization: token $INPUT_REPOTOKEN" \
