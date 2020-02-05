14bdcfdf2c67799f1596f12dd223c6d638d38444
Joe Selman <jsselman@google.com>
2020-02-05 13:47:49 -0800

    Fix issue where set was inverted

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-set -e
-
+set +e
 output=$(find $1 \
     -type f \
     -name \*.yaml \
     -print0 | xargs -0 istioctl analyze -A -d)
 success=$?
 echo $output
+set -e
+
 if [[ $success -ne 0 ]]; then
     # TODO check if PR and only comment if it is
     comments_url=$(jq '.pull_request.comments_url' $GITHUB_EVENT_PATH)
