54848e1e3214dc03d819ce98c5a8e3e044a1a514
Joe Selman <jsselman@google.com>
2020-02-05 13:25:48 -0800

    Experiment with printing comments url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
-#!/bin/sh -l
-find $1 \
+#!/bin/bash
+set -e
+
+output=$(find $1 \
     -type f \
     -name \*.yaml \
-    -print0 | xargs -0 istioctl analyze -A
+    -print0 | xargs -0 istioctl analyze -A -d)
+success=$?
+echo $output
+if [[ $success -ne 0 ]]; then
+    # TODO check if PR and only comment if it is
+    comments_url=$(jq '.pull_request.comments_url' $GITHUB_EVENT_PATH)
+    # TODO actually implement this
+    echo "Comments url: $comments_url"
+fi
